c1b7ef5960e8e319ad65d95f59388503ab88b91c
Fractal393 <54801158+Fractal393@users.noreply.github.com>
2020-09-19 18:26:59 +0530

    Update dev.js

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb+srv://fractal393:test@cluster0.g9jwl.mongodb.net/<dbname>?retryWrites=true&w=majority',
-     }
+  mongoURI:
+    "mongodb+srv://fractal393:test@cluster0.g9jwl.mongodb.net/<dbname>?retryWrites=true&w=majority"
+};
